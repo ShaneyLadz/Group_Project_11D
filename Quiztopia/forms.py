3767cc2171446102dbb91551c24ff040dc8c7283
Jack Neilson <2881679n@student.gla.ac.uk>
2025-03-13 16:52:02 +0000

    Modified forms and created formsets

--- Quiztopia/forms.py
+++ Quiztopia/forms.py
@@ -1,11 +1,12 @@
 from django import forms 
 from Quiztopia.models import Quiz, Question, Answer
+from django.forms import formset_factory, inlineformset_factory
 
 class QuizForm(forms.ModelForm):
 
-    quiz_title = forms.CharField(max_length=200, help_text="Quiz Name:")
-    category = forms.ChoiceField(choices=Quiz.categories, help_text="Category:")
-    difficulty = forms.ChoiceField(choices=Quiz.difficulties, help_text="Difficulty:")
+    quiz_title = forms.CharField(max_length=200)
+    category = forms.ChoiceField(choices=Quiz.categories)
+    difficulty = forms.ChoiceField(choices=Quiz.difficulties)
     upvotes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
@@ -14,7 +15,7 @@ class QuizForm(forms.ModelForm):
 
 class QuestionForm(forms.ModelForm):
 
-    question_text = forms.CharField(max_length=500, help_text="Enter your question")
+    question_text = forms.CharField(widget=forms.HiddenInput(),max_length=500,required=True)
 
     class Meta:
         model = Question
@@ -23,8 +24,11 @@ class QuestionForm(forms.ModelForm):
 class AnswerForm(forms.ModelForm):
 
     answer_text = forms.CharField(max_length=200, help_text="Enter an answer")
-    is_correct = forms.BooleanField()
+    is_correct = forms.BooleanField(required=False,widget=forms.RadioSelect())
 
     class Meta:
         model = Answer
-        fields = ('answer_text',)
+        fields = ('answer_text','is_correct')
+
+QuestionFormSet = formset_factory(QuestionForm, extra=10,min_num=0,max_num=10)
+AnswerFormSet = inlineformset_factory(Question, Answer, form=AnswerForm, extra=4, max_num=4, can_delete=False)
