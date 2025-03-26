7152e203570479c649341f0f96babd60413b4ec9
Jack Neilson <2881679n@student.gla.ac.uk>
2025-03-26 23:26:15 +0000

    Created edit quiz page

--- Quiztopia/urls.py
+++ Quiztopia/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('categories/<str:category_slug>/<int:quiz_id>/take_quiz/quiz_results/', views.quiz_results, name = "quiz_results"),
     path('logout/', views.user_logout, name='logout'),
     path('login/profile', views.profile, name='profile'),
+    path('edit_quiz/<int:quiz_id>/', views.edit_quiz, name='edit_quiz'),
 ]
